a178b7a6a200aaafa3164b1173b695e1d48fa3af
Pedro Henrique <pedro.henrique59@outlook.com>
2022-12-14 08:47:07 -0300

    criando component e service.

---
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jogo-da-velha',
+  templateUrl: './jogo-da-velha.component.html',
+  styleUrls: ['./jogo-da-velha.component.css']
+})
+export class JogoDaVelhaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
